7a041f210f4b3aa8d31a980addaae32d59367cf8
brfish <1748065414@qq.com>
2021-03-20 22:51:17 +0800

    Add more math constants and inline variables

--- include/fmath/constants.h
+++ include/fmath/constants.h
@@ -10,15 +10,7 @@ namespace number
 
 template<typename T>
 struct Pi
-{
-    static constexpr T value = 3;
-};
-
-template<>
-struct Pi<float>
-{
-    static constexpr float value = 3.14159265f;
-};
+{};
 
 template<>
 struct Pi<double>
@@ -26,17 +18,15 @@ struct Pi<double>
     static constexpr double value = 3.141592653589793;
 };
 
-template<typename T>
-struct Pi2
+template<>
+struct Pi<float>
 {
-    static constexpr T value = 2;
+    static constexpr float value = static_cast<float>(Pi<double>::value);
 };
 
-template<>
-struct Pi2<float>
-{
-    static constexpr float value = 1.57079632f;
-};
+template<typename T>
+struct Pi2
+{};
 
 template<>
 struct Pi2<double>
@@ -44,22 +34,106 @@ struct Pi2<double>
     static constexpr double value = 1.570796326794896;
 };
 
-template<typename T>
-struct Pi4
+template<>
+struct Pi2<float>
 {
-    static constexpr T value = 1;
+    static constexpr float value = static_cast<float>(Pi2<double>::value);
 };
 
-template<>
-struct Pi4<float>
-{
-    static constexpr float value = 1.57079632f;
-};
+template<typename T>
+struct Pi4
+{};
 
 template<>
 struct Pi4<double>
 {
     static constexpr double value = 1.570796326794896;
+};
+
+template<>
+struct Pi4<float>
+{
+    static constexpr float value = static_cast<float>(Pi4<double>::value);
+};
+
+template<typename T>
+struct ConstantE
+{};
+
+template<>
+struct ConstantE<double>
+{
+    static constexpr double value = 2.718281828459045;
+};
+
+template<>
+struct ConstantE<float>
+{
+    static constexpr float value = static_cast<float>(ConstantE<double>::value);
+};
+
+template<typename T>
+struct Ln2
+{};
+
+template<>
+struct Ln2<double>
+{
+    static constexpr double value = 0.6931471805599453;
+};
+
+template<>
+struct Ln2<float>
+{
+    static constexpr float value = static_cast<float>(Ln2<double>::value);
+};
+
+template<typename T>
+struct Ln10
+{};
+
+template<>
+struct Ln10<double>
+{
+    static constexpr double value = 2.302585092994046;
+};
+
+template<>
+struct Ln10<float>
+{
+    static constexpr float value = static_cast<float>(Ln10<double>::value);
+};
+
+template<typename T>
+struct Sqrt2
+{};
+
+template<>
+struct Sqrt2<double>
+{
+    static constexpr double value = 1.4142135623730951;
+};
+
+template<>
+struct Sqrt2<float>
+{
+    static constexpr float value = static_cast<float>(Sqrt2<double>::value);
+};
+
+template<typename T>
+struct Sqrt3
+{};
+
+template<>
+struct Sqrt3<double>
+{
+    static constexpr double value = 1.7320508075688772;
+};
+
+template<>
+struct Sqrt3<float>
+{
+    static constexpr float value = static_cast<float>(Sqrt3<double>::value);
 };
 
 template<typename T>
@@ -69,53 +143,34 @@ struct Epsilon
 };
 
 template<>
+struct Epsilon<double>
+{
+    static constexpr double value = static_cast<double>(2.22044604925031308084726333618164062e-16L);
+};
+
+template<>
 struct Epsilon<float>
 {
     static constexpr float value = 1.19209289550781250000000000000000000e-7F;
 };
 
-template<>
-struct Epsilon<double>
-{
-    static constexpr double value = double(2.22044604925031308084726333618164062e-16L);
-};
+inline constexpr double PI = Pi<double>::value;
 
-template<typename T>
-struct Euler
-{
-    static constexpr T value = 3;
-};
+inline constexpr double PI2 = Pi2<double>::value;
 
-template<>
-struct Euler<float>
-{
-    static constexpr float value = 	2.7182818284f;
-};
+inline constexpr double PI4 = Pi4<double>::value;
 
-template<>
-struct Euler<double>
-{
-    static constexpr double value = 2.71828182845904523536;
-};
+inline constexpr double E = ConstantE<double>::value;
 
-template<typename T>
-struct Ln2
-{};
+inline constexpr double LN2 = Ln2<double>::value;
 
-template<typename T> 
-inline constexpr T PI = Pi<T>::value;
+inline constexpr double LN10 = Ln10<double>::value;
 
-template<typename T>
-inline constexpr T PI2 = Pi2<T>::value;
+inline constexpr double SQRT2 = Sqrt2<double>::value;
 
-template<typename T>
-inline constexpr T PI4 = Pi4<T>::value;
+inline constexpr double SQRT3 = Sqrt3<double>::value;
 
-template<typename T>
-inline constexpr T EPSILON = Epsilon<T>::value;
-
-template<typename T>
-inline constexpr T E = Euler<T>::value;
+inline constexpr double EPSILON = Epsilon<double>::value;
 
 }
 }
