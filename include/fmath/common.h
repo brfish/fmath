757c861786053f7adfd05cf4f57ccc2395b87eb4
brfish <1748065414@qq.com>
2021-03-25 23:08:45 +0800

    Add vector component

--- include/fmath/common.h
+++ include/fmath/common.h
@@ -34,9 +34,13 @@ using angle_t   =   double;
 
 enum class Axis
 {
-    X,
-    Y,
-    Z
+    X, Y, Z
+};
+
+enum class Component
+{
+    X = 0, Y = 1, Z = 2, W = 3,
+    R = 0, G = 1, B = 2, A = 3,
 };
 
 }
