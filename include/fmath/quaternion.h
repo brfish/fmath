0b70ac4816d1d0e30ff57d47e35d7591f2e9308f
brfish <1748065414@qq.com>
2021-03-30 20:04:38 +0800

    Update quaternion

--- include/fmath/quaternion.h
+++ include/fmath/quaternion.h
@@ -26,11 +26,15 @@ public:
 
     explicit FMATH_CONSTEXPR Quat(const ValueType &s, const Vector3<ValueType> &v);
 
-    explicit FMATH_CONSTEXPR Quat(Axis axis, ValueType theta);
+    explicit FMATH_CONSTEXPR Quat(const ValueType &s);
 
-    explicit FMATH_CONSTEXPR Quat(const Vector3<ValueType> &axis, angle_t theta);
+    template<typename VectorT,
+        typename = std::enable_if_t<std::is_base_of_v<internal::VectorInterface, VectorT> && VectorT::SIZE == 3>>
+    explicit FMATH_CONSTEXPR Quat(const VectorT &v);
 
     FMATH_CONSTEXPR Quat &operator=(const Quat &other);
+
+    FMATH_CONSTEXPR Quat &operator=(const Vector3<T> &v);
     
     FMATH_INLINE FMATH_CONSTEXPR size_t size() const;
 
@@ -52,9 +56,15 @@ public:
     
     FMATH_INLINE FMATH_CONSTEXPR Vector3<T> axis() const;
 
+    FMATH_INLINE FMATH_CONSTEXPR Quat operator+() const;
+
+    FMATH_INLINE FMATH_CONSTEXPR Quat operator-() const;
+
     FMATH_INLINE Quat &operator+=(const Quat &other);
 
     FMATH_INLINE Quat &operator-=(const Quat &other);
+
+    FMATH_INLINE Quat &operator*=(const Quat &other);
 
     FMATH_INLINE Quat &operator*=(const ValueType &value);
 
@@ -64,10 +74,12 @@ public:
 
     static FMATH_CONSTEXPR Quat identity();
 
+    static FMATH_CONSTEXPR Quat makeRotation(const Vector3<T> &axis, const T &angle);
+
 public:
     union
     {
-        std::array<ValueType, 4> values;
+        Vector4<T> values;
         struct { ValueType w, x, y, z; };
         struct { ValueType s; Vector3<T> v; };
     };
@@ -101,10 +113,10 @@ FMATH_INLINE FMATH_CONSTEXPR bool notEqual(const Quat<T> &q1, const Quat<T> &q2)
 template<typename T>
 FMATH_INLINE FMATH_CONSTEXPR bool equalEpsilon(const Quat<T> &q1, const Quat<T> &q2, const T &epsilon = constants::Epsilon<T>::value)
 {
-    return fmath::equalEpsilon(q1[0], q2[0]) &&
-        fmath::equalEpsilon(q1[1], q2[1]) &&
-        fmath::equalEpsilon(q1[2], q2[2]) &&
-        fmath::equalEpsilon(q1[3], q2[3]);
+    return fmath::equalEpsilon(q1[0], q2[0], epsilon) &&
+        fmath::equalEpsilon(q1[1], q2[1], epsilon) &&
+        fmath::equalEpsilon(q1[2], q2[2], epsilon) &&
+        fmath::equalEpsilon(q1[3], q2[3], epsilon);
 }
 
 template<typename T>
@@ -142,6 +154,12 @@ FMATH_INLINE FMATH_CONSTEXPR Quat<T> operator*(const Quat<T> &q, const T &value)
 }
 
 template<typename T>
+FMATH_INLINE FMATH_CONSTEXPR Quat<T> operator*(const T &value, const Quat<T> &q)
+{
+    return q * value;
+}
+
+template<typename T>
 FMATH_INLINE FMATH_CONSTEXPR Quat<T> operator/(const Quat<T> &q, const T &value)
 {
     FMATH_ASSERT(value != 0);
@@ -176,13 +194,13 @@ FMATH_INLINE FMATH_CONSTEXPR T length2(const Quat<T> &q)
 }
 
 template<typename T>
-T length(const Quat<T> &q)
+FMATH_INLINE FMATH_CONSTEXPR T length(const Quat<T> &q)
 {
     return sqrt(length2(q));
 }
 
 template<typename T>
-Quat<T> normalize(const Quat<T> &q)
+FMATH_INLINE FMATH_CONSTEXPR Quat<T> normalize(const Quat<T> &q)
 {
     return q / length(q);
 }
@@ -191,6 +209,32 @@ template<typename T>
 FMATH_INLINE FMATH_CONSTEXPR Quat<T> inverse(const Quat<T> &q)
 {
     return conjugate(q) / length2(q);
+}
+
+template<typename T>
+FMATH_INLINE FMATH_CONSTEXPR Quat<T> opposite(const Quat<T> &q)
+{
+    return -q;
+}
+
+template<typename T>
+FMATH_INLINE FMATH_CONSTEXPR Quat<T> log(const Quat<T> &q)
+{
+    T alpha = q.angle() / static_cast<T>(2);
+    return Quat<T>(0, alpha * q.axis());
+}
+
+template<typename T>
+FMATH_INLINE FMATH_CONSTEXPR Quat<T> exp(const Quat<T> &q)
+{
+    T alpha = q.angle() / static_cast<T>(2);
+    return Quat<T>(cos(alpha), sin(alpha) * q.axis());
+}
+
+template<typename T>
+FMATH_INLINE FMATH_CONSTEXPR Quat<T> pow(const Quat<T> &q, const T &t)
+{
+    return exp(t * log(q));
 }
 
 template<typename T>
@@ -227,9 +271,29 @@ FMATH_CONSTEXPR Quat<T>::Quat(const ValueType &s, const Vector3<ValueType> &v)
 {}
 
 template<typename T>
+FMATH_CONSTEXPR Quat<T>::Quat(const ValueType &s)
+    :   values { s, static_cast<T>(0), static_cast<T>(0), static_cast<T>(0) }
+{}
+
+template<typename T>
+    template<typename VectorT, typename>
+FMATH_CONSTEXPR Quat<T>::Quat(const VectorT &v)
+    :   values { static_cast<T>(0), v[0], v[1], v[2] }
+{}
+
+template<typename T>
 FMATH_CONSTEXPR Quat<T> &Quat<T>::operator=(const Quat &other)
 {
     values = other.values;
+    return *this;
+}
+
+template<typename T>
+FMATH_CONSTEXPR Quat<T> &Quat<T>::operator=(const Vector3<T> &v)
+{
+    values[0] = v[0];
+    values[1] = v[1];
+    values[2] = v[2];
     return *this;
 }
 
@@ -282,6 +346,33 @@ FMATH_INLINE FMATH_CONSTEXPR typename Quat<T>::ValueType &Quat<T>::operator[](in
 }
 
 template<typename T>
+FMATH_INLINE FMATH_CONSTEXPR typename Quat<T>::ValueType Quat<T>::angle() const
+{
+    return static_cast<T>(2) * acos(values[0]);
+}
+
+template<typename T>   
+FMATH_INLINE FMATH_CONSTEXPR Vector3<T> Quat<T>::axis() const
+{
+    return Vector3<T>(static_cast<T>(2) * asin(values[1]),
+        static_cast<T>(2) * asin(values[2]),
+        static_cast<T>(2) * asin(values[3])
+    );
+}
+
+template<typename T>
+FMATH_INLINE FMATH_CONSTEXPR Quat<T> Quat<T>::operator+() const
+{
+    return Quat(*this);
+}
+
+template<typename T>
+FMATH_INLINE FMATH_CONSTEXPR Quat<T> Quat<T>::operator-() const
+{
+    return Quat(-values[0], -values[1], -values[2], -values[3]);
+}
+
+template<typename T>
 FMATH_INLINE Quat<T> &Quat<T>::operator+=(const Quat &other)
 {
     *this = (*this) + other;
@@ -292,6 +383,13 @@ template<typename T>
 FMATH_INLINE Quat<T> &Quat<T>::operator-=(const Quat &other)
 {
     *this = (*this) + other;
+    return *this;
+}
+
+template<typename T>
+FMATH_INLINE Quat<T> &Quat<T>::operator*=(const Quat &other)
+{
+    *this = (*this) * other;
     return *this;
 }
 
@@ -312,17 +410,52 @@ FMATH_INLINE Quat<T> &Quat<T>::operator/=(const ValueType &value)
 template<typename T>
 FMATH_INLINE FMATH_CONSTEXPR Matrix4<T> Quat<T>::toMatrix() const
 {
-    return Matrix4<T>(values[0], values[3], -values[2], -values[1],
-        -values[3], values[0], values[1], -values[2],
-        values[2], -values[1], values[0], -values[3],
-        values[1], values[2], values[3], values[0]
-    );
+    Quat<T> q = normalize(*this);
+
+    T wx = q[0] * q[1];
+    T wy = q[0] * q[2];
+    T wz = q[0] * q[3];
+
+    T xx = q[1] * q[1];
+    T xy = q[1] * q[2];
+    T xz = q[1] * q[3];
+
+    T yy = q[2] * q[2];
+    T yz = q[2] * q[3];
+
+    T zz = q[3] * q[3];
+
+    const T one = static_cast<T>(1);
+    const T two = static_cast<T>(2);
+
+    Matrix4<T> r;
+    r[0][0] = one - two * (yy + zz);
+    r[0][1] = two * (xy + wz);
+    r[0][2] = two * (xz - wy);
+
+    r[1][0] = two * (xy - wz);
+    r[1][1] = one - two * (xx + zz);
+    r[1][2] = two * (yz + wx);
+
+    r[2][0] = two * (xz - wy);
+    r[2][1] = two * (yz - wx);
+    r[2][2] = one - two * (xx + yy);
+
+    r[3][3] = one;
+
+    return r;
 }
 
 template<typename T>
 FMATH_CONSTEXPR Quat<T> Quat<T>::identity()
 {
     return Quat<T>(1, 0, 0, 0);
+}
+
+template<typename T>
+FMATH_CONSTEXPR Quat<T> Quat<T>::makeRotation(const Vector3<T> &axis, const T &angle)
+{
+    return Quat<T>(cos(angle / static_cast<T>(2)), axis * sin(angle / static_cast<T>(2)));
 }
 
 using Quatf = Quat<float>;
