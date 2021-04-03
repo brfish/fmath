a8e4c3479baac8c128f3c256cecaba9ac7134baf
brfish <1748065414@qq.com>
2021-04-03 21:58:23 +0800

    Add Line and Ray

---
+++ include/fmath/line.h
@@ -0,0 +1,248 @@
+#ifndef _FMATH_LINE_H_
+#define _FMATH_LINE_H_
+
+#include "common.h"
+#include "constants.h"
+#include "point.h"
+
+namespace fmath
+{
+
+template<typename T, size_t N>
+class Line
+{
+public:
+    using ValueType = T;
+    static constexpr size_t SIZE = N;
+
+public:
+    FMATH_CONSTEXPR Line(const Line &other);
+
+    explicit FMATH_CONSTEXPR Line(const Point<ValueType, N> &start, const Point<ValueType, N> &end);
+
+    FMATH_CONSTEXPR Line &operator=(const Line &other);
+
+    FMATH_INLINE FMATH_CONSTEXPR const Point<ValueType, N> &start() const;
+
+    FMATH_INLINE FMATH_CONSTEXPR Point<ValueType, N> &start();
+
+    FMATH_INLINE FMATH_CONSTEXPR const Point<ValueType, N> &end() const;
+
+    FMATH_INLINE FMATH_CONSTEXPR Point<ValueType, N> &end();
+
+    FMATH_INLINE FMATH_CONSTEXPR Point<ValueType, N> at(const ValueType &t);
+
+    FMATH_INLINE FMATH_CONSTEXPR Point<ValueType, N> middle() const;
+
+    FMATH_INLINE FMATH_CONSTEXPR Vector<ValueType, N> deltaVector() const;
+
+    FMATH_INLINE FMATH_CONSTEXPR Vector<ValueType, N> direction() const;
+
+    FMATH_INLINE FMATH_CONSTEXPR ValueType length2() const;
+
+    FMATH_INLINE FMATH_CONSTEXPR ValueType length() const;
+
+    FMATH_INLINE void setStart(const Point<ValueType, N> &start);
+
+    FMATH_INLINE void setEnd(const Point<ValueType, N> &end);
+
+private:
+    Point<ValueType, SIZE> start_;
+    Point<ValueType, SIZE> end_;
+};
+
+template<typename T, size_t N>
+bool operator==(const Line<T, N> &l1, const Line<T, N> &l2)
+{
+    return l1.start() == l2.start() && l1.end() == l2.end();
+}
+
+template<typename T, size_t N>
+bool operator!=(const Line<T, N> &l1, const Line<T, N> &l2)
+{
+    return !(l1 == l2);
+}
+
+template<typename T, size_t N>
+bool equal(const Line<T, N> &l1, const Line<T, N> &l2)
+{
+    return l1 == l2;
+}
+
+template<typename T, size_t N>
+bool notEqual(const Line<T, N> &l1, const Line<T, N> &l2)
+{
+    return l1 != l2;
+}
+
+template<typename T, size_t N>
+bool equalEpsilon(const Line<T, N> &l1, const Line<T, N> &l2, const T &epsilon = constants::Epsilon<T>::value)
+{
+    return equalEpsilon(l1.start(), l2.start(), epsilon) &&
+        equalEpsilon(l1.end(), l2.end(), epsilon);
+}
+
+template<typename T, size_t N>
+bool notEqualEpsilon(const Line<T, N> &l1, const Line<T, N> &l2, const T &epsilon = constants::Epsilon<T>::value)
+{
+    return !equalEpsilon(l1, l2, epsilon);
+}
+
+template<typename T, size_t N>
+T length2(const Line<T, N> &l)
+{
+    return distance2(l.start(), l.end());
+}
+
+template<typename T, size_t N>
+T length(const Line<T, N> &l)
+{
+    return distance(l.start(), l.end());
+}
+
+template<typename T, size_t N>
+T distance2(const Line<T, N> &l, const Point<T, N> &p)
+{
+    Vector<T, N> d = l.start() - p;
+    Vector<T, N> n = l.direction();
+
+    return d - (d * n) * n;
+}
+
+template<typename T, size_t N>
+T distance2(const Point<T, N> &p, const Line<T, N> &l)
+{
+    return distance2(l, p);
+}
+
+template<typename T, size_t N>
+T distance(const Line<T, N> &l, const Point<T, N> &p)
+{
+    Vector<T, N> d = l.start() - p;
+    Vector<T, N> n = l.direction();
+
+    return d - (d * n) * n;
+}
+
+template<typename T, size_t N>
+T distance(const Point<T, N> &p, const Line<T, N> &l)
+{
+    return distance(l, p);
+}
+
+template<typename T, size_t N>
+FMATH_INLINE std::ostream &operator<<(std::ostream &output, const Line<T, N> &l)
+{
+    output << '(' << l.start() << ',' << l.end() << ')';
+    
+    return output;
+}
+
+template<typename T, size_t N>
+FMATH_CONSTEXPR Line<T, N>::Line(const Line &other)
+    :   start_(other.start_),
+        end_(other.end_)
+{}
+
+template<typename T, size_t N>
+FMATH_CONSTEXPR Line<T, N>::Line(const Point<ValueType, N> &start, const Point<ValueType, N> &end)
+    :   start_(start),
+        end_(end)
+{}
+
+template<typename T, size_t N>
+FMATH_CONSTEXPR Line<T, N> &Line<T, N>::operator=(const Line &other)
+{
+    start_ = other.start_;
+    end_ = other.end_;
+    
+    return *this;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR const Point<T, N> &Line<T, N>::start() const
+{
+    return start_;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR Point<T, N> &Line<T, N>::start()
+{
+    return start_;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR const Point<T, N> &Line<T, N>::end() const
+{
+    return end_;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR Point<T, N> &Line<T, N>::end()
+{
+    return end_;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR Point<T, N> Line<T, N>::at(const ValueType &t)
+{
+    return start_ + (end_ - start_) * t;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR Point<T, N> Line<T, N>::middle() const
+{
+    return at(static_cast<ValueType>(0.5));
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR Vector<T, N> Line<T, N>::deltaVector() const
+{
+    return end_ - start_;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR Vector<T, N> Line<T, N>::direction() const
+{
+    return normalize(deltaVector());
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR T Line<T, N>::length2() const
+{
+    return distance2(start_, end_);
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR T Line<T, N>::length() const
+{
+    return distance(start_, end_);
+}
+
+template<typename T, size_t N>
+FMATH_INLINE void Line<T, N>::setStart(const Point<ValueType, N> &start)
+{
+    start_ = start;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE void Line<T, N>::setEnd(const Point<ValueType, N> &end)
+{
+    end_ = end;
+}
+
+template<typename T>
+using Line2 = Line<T, 2>;
+
+template<typename T>
+using Line3 = Line<T, 3>;
+
+using Line2f = Line2<float>;
+using Line2lf = Line2<double>;
+
+using Line3f = Line3<float>;
+using Line3lf = Line3<double>;
+
+}
+
+#endif
---
+++ include/fmath/ray.h
@@ -0,0 +1,173 @@
+#ifndef _FMATH_RAY_H_
+#define _FMATH_RAY_H_
+
+#include "point.h"
+#include "vector.h"
+#include "line.h"
+
+namespace fmath
+{
+
+template<typename T, size_t N>
+class Ray
+{
+public:
+    using ValueType = T;
+    static constexpr size_t SIZE = N;
+
+public:
+    FMATH_CONSTEXPR Ray(const Ray &other);
+
+    explicit FMATH_CONSTEXPR Ray(const Point<ValueType, N> &origin, const Vector<ValueType, N> &direction);
+
+    FMATH_CONSTEXPR Ray &operator=(const Ray &other);
+
+    FMATH_INLINE FMATH_CONSTEXPR const Point<T, N> &origin() const;
+
+    FMATH_INLINE FMATH_CONSTEXPR Point<T, N> &origin();
+
+    FMATH_INLINE FMATH_CONSTEXPR const Vector<T, N> &direction() const;
+
+    FMATH_INLINE FMATH_CONSTEXPR Vector<T, N> &direction();
+
+    FMATH_INLINE FMATH_CONSTEXPR Point<T, N> at(const ValueType &t);
+
+    FMATH_INLINE FMATH_CONSTEXPR Point<T, N> operator()(const ValueType &t);
+
+    FMATH_INLINE FMATH_CONSTEXPR Line<T, N> toLine(const ValueType &t0, const ValueType &t1);
+
+    FMATH_INLINE void setOrigin(const Point<T, N> &origin);
+
+    FMATH_INLINE void setDirection(const Vector<T, N> &direction);
+
+private:
+    Point<ValueType, SIZE> origin_;
+    Vector<ValueType, SIZE> direction_;
+};
+
+template<typename T, size_t N>
+bool operator==(const Ray<T, N> &r1, const Ray<T, N> &r2)
+{
+    return r1.origin() == r2.origin() && r1.direction() == r2.direction();
+}
+
+template<typename T, size_t N>
+bool operator!=(const Ray<T, N> &r1, const Ray<T, N> &r2)
+{
+    return !(r1 == r2);
+}
+
+template<typename T, size_t N>
+bool equal(const Ray<T, N> &r1, const Ray<T, N> &r2)
+{
+    return r1 == r2;
+}
+
+template<typename T, size_t N>
+bool notEqual(const Ray<T, N> &r1, const Ray<T, N> &r2)
+{
+    return r1 != r2;
+}
+
+template<typename T, size_t N>
+bool equalEpsilon(const Ray<T, N> &r1, const Ray<T, N> &r2, const T &epsilon = constants::Epsilon<T>::value)
+{
+    return equalEpsilon(r1.origin(), r2.origin(), epsilon) &&
+        equalEpsilon(r1.direction(), r2.direction(), epsilon);
+}
+
+template<typename T, size_t N>
+bool notEqualEpsilon(const Ray<T, N> &r1, const Ray<T, N> &r2, const T &epsilon = constants::Epsilon<T>::value)
+{
+    return !equalEpsilon(r1, r2, epsilon);
+}
+
+template<typename T, size_t N>
+FMATH_CONSTEXPR Ray<T, N>::Ray(const Ray &other)
+    :   origin_(other.origin_),
+        direction_(other.direction_)
+{}
+
+template<typename T, size_t N>
+FMATH_CONSTEXPR Ray<T, N>::Ray(const Point<T, N> &origin, const Vector<T, N> &direction)
+    :   origin_(origin),
+        direction_(direction)
+{}
+
+template<typename T, size_t N>
+FMATH_CONSTEXPR Ray<T, N> &Ray<T, N>::operator=(const Ray &other)
+{
+    origin_ = other.origin_;
+    direction_ = other.direction_;
+    return *this;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR const Point<T, N> &Ray<T, N>::origin() const
+{
+    return origin_;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR Point<T, N> &Ray<T, N>::origin()
+{
+    return origin_;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR const Vector<T, N> &Ray<T, N>::direction() const
+{
+    return direction_;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR Vector<T, N> &Ray<T, N>::direction()
+{
+    return direction_;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR Point<T, N> Ray<T, N>::at(const ValueType &t)
+{
+    return origin_ + direction_ * t;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR Point<T, N> Ray<T, N>::operator()(const ValueType &t)
+{
+    return at(t);
+}
+
+template<typename T, size_t N>
+FMATH_INLINE FMATH_CONSTEXPR Line<T, N> Ray<T, N>::toLine(const ValueType &t0, const ValueType &t1)
+{
+    return Line<T, N>(origin_ + direction_ * t0, origin_ + direction_ * t1);
+}
+
+template<typename T, size_t N>
+FMATH_INLINE void Ray<T, N>::setOrigin(const Point<T, N> &origin)
+{
+    origin_ = origin;
+}
+
+template<typename T, size_t N>
+FMATH_INLINE void Ray<T, N>::setDirection(const Vector<T, N> &direction)
+{
+    direction_ = normalize(direction);
+}
+
+template<typename T>
+using Ray2 = Ray<T, 2>;
+
+template<typename T>
+using Ray3 = Ray<T, 3>;
+
+using Ray2f = Ray2<float>;
+using Ray3lf = Ray2<double>;
+
+using Ray3f = Ray3<float>;
+using Ray3lf = Ray3<double>;
+
+}
+
+#endif
