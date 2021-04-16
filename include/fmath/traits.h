2e03e9b3e08423997dba8a2b3c29190f4ed4bf36
brfish <1748065414@qq.com>
2021-04-16 18:04:32 +0800

    Update: extend is_floating_pointer and is_integral to support more types

--- include/fmath/traits.h
+++ include/fmath/traits.h
@@ -8,6 +8,19 @@
 namespace fmath
 {
 
+namespace internal
+{
+
+template<typename T, typename = void>
+struct HasValueType : std::false_type
+{};
+
+template<typename T>
+struct HasValueType<T, std::void_t<typename T::ValueType>> : std::true_type
+{};
+
+}
+
 template<typename T>
 struct IsVector : std::conditional_t<
     std::is_base_of_v<internal::VectorInterface, T>, 
@@ -22,11 +35,35 @@ struct IsMatrix : std::conditional_t<
         std::false_type>
 {};
 
+template<typename T, typename = void>
+struct IsFloatingPoint : std::is_floating_point<T>
+{};
+
+template<typename T>
+struct IsFloatingPoint<T, std::enable_if_t<internal::HasValueType<T>::value>> :
+    std::is_floating_point<typename T::ValueType>
+{};
+
+template<typename T, typename = void>
+struct IsIntegral : std::is_integral<T>
+{};
+
+template<typename T>
+struct IsIntegral<T, std::enable_if_t<internal::HasValueType<T>::value>> :
+    std::is_integral<typename T::ValueType>
+{};
+
 template<typename T>
 inline constexpr bool is_vector_v = IsVector<T>::value;
 
 template<typename T>
 inline constexpr bool is_matrix_v = IsMatrix<T>::value;
+
+template<typename T>
+inline constexpr bool is_floating_point_v = IsFloatingPoint<T>::value;
+
+template<typename T>
+inline constexpr bool is_integral_v = IsIntegral<T>::value;
 
 }
 
