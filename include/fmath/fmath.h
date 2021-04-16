038fb1f6d0bd63886abaab614eab6ccb39ba380a
brfish <1748065414@qq.com>
2021-04-16 17:56:56 +0800

    New: create fmath.h

---
+++ include/fmath/fmath.h
@@ -0,0 +1,26 @@
+#ifndef _FMATH_H_
+#define _FMATH_H_
+
+// Just include all headers
+
+#include "box.h"
+#include "color.h"
+#include "common.h"
+#include "constants.h"
+#include "line.h"
+#include "math_common_functions.h"
+#include "matrix.h"
+#include "normal.h"
+#include "plane.h"
+#include "point.h"
+#include "quaternion.h"
+#include "random.h"
+#include "ray.h"
+#include "sphere.h"
+#include "swizzle.h"
+#include "traits.h"
+#include "transform.h"
+#include "triangle.h"
+#include "vector.h"
+
+#endif
